69dee591708f8a93329e8730a77216537458835d
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-29 21:21:51 -0700

    check get function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """ test work with redis """
 import redis
 import uuid
+from typing import Union, Callable
 
 
 class Cache():
@@ -11,8 +12,32 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, value: any) -> str:
-        """stroe redis value in uuid and return uuid"""
+    def store(self, data: any) -> str:
+        """store redis value in uuid and return uuid"""
         key = str(uuid.uuid4())
-        self._redis.set(key, value)
+        self._redis.set(key, data)
         return key
+
+    def get_int(self, value: int) -> int:
+        """Convert into int ant return it"""
+        x = int(value)
+        return x
+
+    def get_str(self, value: str) -> str:
+        """Convert to string and return it"""
+        x = str(value)
+        return x
+
+    def get(self, key: str, fn:  Union[str, bytes, Callable, int, float, None]) -> Union[str, bytes, int, float, None]:
+        """Check the type on fn from the dict then retrive convert"""
+        value = self._redis.get(key)
+        if fn == int:
+            x = self.get_int(value)
+            return x
+        if fn == str:
+            x = self.get_str(value)
+            return x
+        if callable(fn):
+            return fn(value)
+        if fn is None:
+            return value
