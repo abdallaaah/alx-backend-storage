2ebdc0dd60d8b3e4055d46d054a7b613bdb707d3
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-30 17:35:56 -0700

    check exercise call history

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,6 +15,17 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self: Any, *args):
+        x = method.__qualname__
+        print(f"{str(args)} zzzzzzzzzz")
+        self._redis.rpush(f'{method.__qualname__}:inputs', str(args))
+        key = method(self, str(args))
+        self._redis.rpush(f"{method.__qualname__}:outputs", key)
+    return wrapper
+
+
 class Cache():
     """class cash redponsilbe for redis connect"""
     def __init__(self):
@@ -23,6 +34,7 @@ class Cache():
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data:  Union[str, bytes, int, float]) -> str:
         """store redis value in uuid and return uuid"""
         key = str(uuid.uuid4())
@@ -39,6 +51,7 @@ class Cache():
         x = str(value)
         return x
 
+    @call_history
     def get(self, key: str, fn:  Optional[Callable] = None) -> Any:
         """Check the type on fn from the dict then retrive convert"""
         value = self._redis.get(key)
