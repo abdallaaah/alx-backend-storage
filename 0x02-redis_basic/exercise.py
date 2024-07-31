3439f30c4132ce19a75798a9faafedce2eff4fa2
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-30 18:00:00 -0700

    check task range

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,9 +17,9 @@ def count_calls(method: Callable) -> Callable:
 
 def call_history(method: Callable) -> Callable:
     @wraps(method)
-    def wrapper(self: Any, *args):
+    def wrapper(self: Any, *args, **kwags):
         self._redis.rpush(f'{method.__qualname__}:inputs', str(args))
-        key = method(self, *args)
+        key = method(self, *args, **kwags)
         self._redis.rpush(f"{method.__qualname__}:outputs", key)
         return key
     return wrapper
